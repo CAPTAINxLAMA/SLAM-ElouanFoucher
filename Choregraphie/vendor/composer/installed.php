f7d015762e043454a8afb5a6806c40d5c983e51c
Elouan <elouanfou@gmail.com>
2025-12-12 12:15:20 +0100

    Redémarrage de 0 : composer installé et projet platform IO recrée

---
+++ Choregraphie/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '6996d8a36c8e3850f2fe75b41432d8e82f70587a',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../SLAM/',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '6996d8a36c8e3850f2fe75b41432d8e82f70587a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../SLAM/',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'bluerhinos/phpmqtt' => array(
+            'pretty_version' => '1.0.1',
+            'version' => '1.0.1.0',
+            'reference' => 'fe4b6b2fe3d1b651fe1456e147ad4f044fa70603',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../bluerhinos/phpmqtt',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
